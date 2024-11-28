ea1c11521be6240d4d81cc74d2d8beaee7ccc609
Anton Yermakovich <antonyermakovich@gmail.com>
2024-11-28 22:41:02 +0300

    init project

---
+++ assets/js/app.js
@@ -0,0 +1,93 @@
+// Sliders
+new Splide("#splide-partners", {
+  perPage: 5,
+  rewind: true,
+  pagination: false,
+  arrows: false,
+  type: "loop",
+  drag: "free",
+  autoScroll: {
+    speed: 1,
+  },
+}).mount(window.splide.Extensions);
+
+new Splide("#portfolio-splide", {
+  perPage: 3,
+  perMove: 1,
+  rewind: true,
+  pagination: true,
+  arrows: false,
+  gap: 20,
+}).mount();
+
+// Changing the class when scrolling
+window.addEventListener("scroll", () => {
+  let scrollDistance = window.scrollY;
+  const header = document.querySelector(".header");
+
+  document.querySelectorAll(".section").forEach((el, i) => {
+    if (el.offsetTop - header.clientHeight <= scrollDistance) {
+      document.querySelectorAll(".menu__link").forEach((link) => {
+        if (link.classList.contains("menu__link_active")) {
+          link.classList.remove("menu__link_active");
+        }
+      });
+
+      document
+        .querySelectorAll(".menu__link")
+        [i].classList.add("menu__link_active");
+    }
+  });
+
+  scrollDistance >= 1
+    ? header.classList.add("header_sticky")
+    : header.classList.remove("header_sticky");
+});
+
+// forms
+document
+  .querySelectorAll("form")
+  .forEach((form) =>
+    form.addEventListener("submit", (e) => e.preventDefault())
+  );
+
+// Open the modal
+const modals = document.querySelectorAll(".modal-delivery");
+document
+  .querySelectorAll(".btn_more")
+  .forEach((btn) => btn.addEventListener("click", openModal));
+
+function openModal() {
+  const currentModal = modals[this.dataset.delivery];
+  document.body.classList.add("hidden");
+  currentModal.classList.remove("modal_hidden");
+
+  currentModal.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("modal_close") ||
+      e.target.classList.contains("modal__overlay")
+    ) {
+      document.body.classList.remove("hidden");
+      currentModal.classList.add("modal_hidden");
+    }
+    return;
+  });
+}
+
+document.querySelector(".consultation__btn").addEventListener("click", () => {
+  const modalConsultation = document.querySelector(".modal__consultation");
+
+  document.body.classList.add("hidden");
+  modalConsultation.classList.remove("modal_hidden");
+
+  modalConsultation.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("modal_close") ||
+      e.target.classList.contains("modal__overlay")
+    ) {
+      document.body.classList.remove("hidden");
+      modalConsultation.classList.add("modal_hidden");
+    }
+    return;
+  });
+});
